5f4339e28d5e50b958498028d7e8e5882f04df27
star.stalker9160 <abyazjschool@gmail.com>
2025-03-20 07:27:35 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from threading import Timer
+from backend.helper import *
+from backend.exceptions import *
+from webbrowser import open as webbrowser_open
+from flask import (
+    Flask,
+    render_template
+)
+
+
+app = Flask(__name__)
+print("[  OK  ] Starting app...")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def not_found_404(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(405)
+def not_found_405(e):
+    return render_template("405.html"), 405
+
+
+if __name__ == "__main__":
+    try:
+        k = "[" + foreplay() + "]"
+        if k not in ["[I-11]", "[I-22]", "[I-12]", "[I-21]"]:
+            raise InitializationErr(f"App unable to initialize, failed with error code: {k}")
+        else:
+            print(f"[  OK  ] App initialized successfully; code: {k}")
+
+        app.run(host="127.0.0.1", port=8080, debug=False)
+        # Timer(1, lambda: webbrowser_open("http://127.0.0.1:8080")).start()
+    except InitializationErr as e:
+        print(f"[ FAIL ] Initialization error: {e}")
+    except Exception as e:
+        print(f"[ FAIL ] Error: {e}")
---
+++ backend/helper.py
@@ -0,0 +1,33 @@
+import os, shutil
+from google import genai
+
+
+def foreplay() -> str:
+    """Foreplay before the good shit, (app initialization):
+        1. Create db if not existing
+        2. Clear pycache to avoid bugs
+    """
+    filepath = "db/.csv"
+    folder = os.path.dirname(filepath)
+
+    out = "I-"
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        out += "2"
+    else:
+        out += "1"
+
+    if not os.path.exists(filepath):
+        with open(filepath, "w") as file:
+            file.write("C\n")
+        out += "2"
+    else:
+        out += "1"
+
+    pycache_path = os.path.join(os.getcwd(), "backend/__pycache__")
+    if os.path.exists(pycache_path):
+        shutil.rmtree(pycache_path)
+
+    return out
+
