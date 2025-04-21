4fd926d6d08e1d379e30374baeb4f18589dd93f0
star.stalker9160 <abyazjschool@gmail.com>
2025-04-21 13:09:24 +0400

    .

--- backend/helper.py
+++ backend/helper.py
@@ -6,29 +6,11 @@ def foreplay() -> str:
         1. Create db if not existing
         2. Clear pycache to avoid bugs
     """
-    filepath = "db/.csv"
-    folder = os.path.dirname(filepath)
-
-    out = "[I-"
-
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-        out += "2"
-    else:
-        out += "1"
-
-    if not os.path.exists(filepath):
-        with open(filepath, "w") as file:
-            file.write("C\n")
-        out += "2"
-    else:
-        out += "1"
-
     pycache_path = os.path.join(os.getcwd(), "backend/__pycache__")
     if os.path.exists(pycache_path):
         shutil.rmtree(pycache_path)
 
-    return f"{out}]"
+    return f"[I-11]]"
 
 
 def log(msg: str, code: int = 0) -> None:
